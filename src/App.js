d6d71c88143e58b95f916217b2246b26dd7c535f
Giovanni Souza <GSMartins999>
2023-10-26 20:23:48 -0300

    revisão

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import HomePage from "./pages/HomePage";
+import { ChakraProvider } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+    const getAllUsers = () => {
+      axios.get("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users", 
+      {headers: {Authorization: "giovanni-souza-krexu"}})
+      .then((response)=> {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      })
+    }
+
+    useEffect(() => {
+      getAllUsers()
+    }, [])
+
   return (
-    <>
-      Exercicio de Router
-    </>
+    <ChakraProvider>
+      {
+      users.map((user) => {
+        return <HomePage key={user.name} name={user.name} id={user.id}/>
+      })
+      } 
+    </ChakraProvider>
   );
 }
 
